2a3ac71af2abdc5e2e74a6d852b852d3a025ecbf
ugen <ugen.kwon@gmail.com>
2019-04-08 21:30:08 +0900

    서브페이지 업데이트

--- src/origin/assets/data/update.js
+++ src/origin/assets/data/update.js
@@ -12,50 +12,50 @@ const update = {
     {
       index : 1,
       type : 'framer',
-      text : '111이번엔 000이 업데이트 되었다옹옹',
-      date : '2019-01-01',
+      text : 'Sticky Header / 스크롤 상단변경 및 App Store 레퍼런스 인터랙션',
+      date : '2019-04-07',
       newIcon : true
     },{
       index : 2,
-      type : 'framerx',
-      text : '222이번엔 000이 업데이트 되었다옹옹',
-      date : '2019-01-01',
+      type : 'framer',
+      text : 'Player Album / Scrollable Drawer 등의 Material backdrop UI',
+      date : '2019-04-04',
       newIcon : true
     },{
       index :3,
       type : 'lottie',
-      text : '3333이번엔 000이 업데이트 되었다옹옹',
-      date : '2019-01-01',
-      newIcon : true
+      text : '지문인식 Animation / Hamburger Menu',
+      date : '2019-04-03',
+      newIcon : false
     },{
       index : 4,
-      type : 'principle',
-      text : '444이번엔 000이 업데이트 되었다옹옹',
-      date : '2019-01-01',
+      type : 'framer',
+      text : 'Contents Generate List 시리즈',
+      date : '2019-04-01',
       newIcon : false
     },{
       index : 5,
       type : 'sketch',
-      text : '55이번엔 000이 업데이트 되었다옹옹',
-      date : '2019-01-01',
+      text : 'Datasoda Splash / Line Animation',
+      date : '2019-04-01',
       newIcon : false
     },{
       index : 6,
-      type : 'HTML',
-      text : '66이번엔 000이 업데이트 되었다옹옹',
-      date : '2019-01-01',
+      type : 'principle',
+      text : 'Mic Effect Animation / Equalizer Animation',
+      date : '2019-03-30',
       newIcon : false
     },{
       index : 7,
-      type : 'framer',
-      text : '77이번엔 000이 업데이트 되었다옹옹',
-      date : '2019-01-01',
+      type : 'framerx',
+      text : 'Parallax Scroll',
+      date : '2019-03-29',
       newIcon : false
     },{
       index : 8,
-      type : 'framer',
-      text : '888이번엔 000이 업데이트 되었다옹옹',
-      date : '2019-01-01',
+      type : 'html',
+      text : 'Volo Splash Animation',
+      date : '2019-03-28',
       newIcon : false
     }
   ]
