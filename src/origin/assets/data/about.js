2db759b831712e18727d819d7d4815b44c61d0d3
ugen <ugen.kwon@gmail.com>
2019-06-11 14:41:31 +0900

    업데이트 이미지 수정

--- src/origin/assets/data/about.js
+++ src/origin/assets/data/about.js
@@ -11,18 +11,18 @@ const about = {
     {
       index:1,
       text : 'Framer Classic을 기준으로 Principle, Lottie 등 다양한 툴을 활용하여 제작한 선행 인터랙션 리스트입니다. 개별 Function 단위로 간략한 설명과 함께 필요한 인터랙션을 빠르게 서치할 수 있습니다. UI Animation gif파일로 직관적인 기능확인과, 인터랙션 구현에 필요한 스크립트를 상세 페이지에 포함시켜 빠른 프로토타이핑을 가능하게 지원하고 있습니다.',
-      imgUrl : 'http://ux.skplanet.com/ic/img_about_01.jpg',
+      imgUrl : 'http://ux.skplanet.com/ic/images/img_about_01.jpg',
       desc : '< Framer Classic을 활용한 인터랙션 파일 >'
     },{
       index:2,
       text : '별도 백엔드 개발없이 간단한 텍스트 입력만으로 추가적인 업데이트 및 관리가 가능하도록 구현했습니다. JS파일에 프로토타이핑 파일의 종류, 인터랙션 성격, 구분을 위한 이미지, 제목 등의 입력을 통해 미리 설정해둔 항목을 작성하면 해당 내용의 데이터 파싱을 통해 간단하게 사이트 적용을 할 수 있습니다. 인터랙션 파일 뿐만 아니라 카탈로그 사이트의 Update, Docs, About 등의 상세페이지들도 각각의 JS파일로 업데이트가 가능합니다.',
-      imgUrl : 'http://ux.skplanet.com/ic/img_about_02.jpg',
+      imgUrl : 'http://ux.skplanet.com/ic/images/img_about_02.jpg',
       desc : '< 업데이트를 위한 코드 추가 및 수정 >'
     }
     ,{
       index:3,
       text : '현재 인터랙션 카탈로그는 1.0 버전으로 전체 로드맵중 UI 컴포넌트와 사용자 이벤트 중심으로 구성이 되어 있습니다.  마이크로인터랙션에 가장 효과적으로 대응이 가능한 Framer를 메인 프로토타이핑 툴로 사용한 이유 입니다. 다음 단계는 개별 화면단위의 인터랙션 뿐만아니라 Task중심의 사용자 Flow를 검증 할 수 있는 Sketch, Principle 을 활용하여 화면간의 Transition 인터랙션을 업데이트할 예정입니다. 추후 사용자 행동 Data들을 사전에 확보하고 가설 검증에 활용 할 수 있도록 Visual Analytics Tool이 적용된 HTML 인터랙션까지 진행한다면  SK Planet UX Asset 으로서의 역할을 충분히 할 수 있을거라 기대합니다.',
-      imgUrl : 'http://ux.skplanet.com/ic/img_about_03.jpg',
+      imgUrl : 'http://ux.skplanet.com/ic/images/img_about_03.jpg',
       desc : '< Interaction Catalog Roadmap >'
     }
   ]
